385dfeb05588460733f6a9fd7c39972a8aad0d45
RahmatulFirdaus <rahmatulfirdausuniska@gmail.com>
2025-06-27 13:41:03 +0800

    updates

--- cron/deleteKeranjangJob.js
+++ cron/deleteKeranjangJob.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const { hapusItemKeranjangKadaluarsa } = require('../controller/apiController');
 
-// Jalankan setiap menit
+// Jalankan jam 24
 cron.schedule('0 0 * * *', async () => {
     console.log(`[⏰ ${new Date().toLocaleString()}] Cron job harian berjalan...`);
     await hapusItemKeranjangKadaluarsa();
